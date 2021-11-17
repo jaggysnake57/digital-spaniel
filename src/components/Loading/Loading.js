d43124a18bb37377275eb10eb0a1284139edb98d
Dean Richards <deanrichardsdeveloper@gmail.com>
2021-11-17 15:34:36 +0000

    Add Loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import loading from '../../assets/loading.gif';
+
+const Loading = () => {
+	return (
+		<div className="loading">
+			<img src={loading} alt="" />
+		</div>
+	);
+};
+
+export default Loading;
